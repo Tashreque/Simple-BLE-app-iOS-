2b92b6fdf0ac47dd10479334096f923ac7e412f6
Tashreque <tashreque@icloud.com>
2020-01-22 10:14:26 +0600

    Added code for Bluetooth connectivity. Peripheral services or characteristics have not yet been handled.

--- BluetoothTesting/ViewController.swift
+++ BluetoothTesting/ViewController.swift
@@ -1,20 +1,71 @@
-//
-//  ViewController.swift
-//  BluetoothTesting
-//
-//  Created by Tashreque on 1/20/20.
-//  Copyright © 2020 Tashreque. All rights reserved.
-//
-
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
+    
+    // Bluetooth central manager.
+    var centralManager: CBCentralManager!
+    
+    // Bluetooth peripheral manager.
+    var somePeripheral: CBPeripheral!
+    
+    //IBOutlet references.
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func bluetoothDidTap(_ sender: UIButton) {
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+}
 
+extension ViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            print("Central state is unknown!")
+        case .resetting:
+            print("Central state is resetting!")
+        case .unsupported:
+            print("Central state is unsupported!")
+        case .unauthorized:
+            print("Central state is unauthorized!")
+        case .poweredOff:
+            print("Central state is poweredOff!")
+        case .poweredOn:
+            print("Central state is poweredOn!")
+            centralManager.scanForPeripherals(withServices: nil)
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print(peripheral)
+        somePeripheral = peripheral
+        central.stopScan()
+        central.connect(somePeripheral, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("Connected to peripheral: \(peripheral)")
+        peripheral.discoverServices(nil)
+    }
+}
+
+extension ViewController: CBPeripheralDelegate {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else {
+            return
+        }
+        
+        services.forEach { (service) in
+            print(service)
+        }
+    }
 }
 
